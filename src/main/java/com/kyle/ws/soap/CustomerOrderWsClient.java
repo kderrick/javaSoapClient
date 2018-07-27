fb1c207225e02f6d10cb21fed24eecadbaa50dac
Kyle <kderrick1988@gmail.com>
2018-07-27 08:22:42 -0700

    update CustomerOrderWsClient to include try/catch and to get all products within orders and print to console

--- src/main/java/com/kyle/ws/soap/CustomerOrderWsClient.java
+++ src/main/java/com/kyle/ws/soap/CustomerOrderWsClient.java
@@ -9,19 +9,34 @@ import com.bharath.ws.trainings.CustomerOrdersPortType;
 import com.bharath.ws.trainings.GetOrdersRequest;
 import com.bharath.ws.trainings.GetOrdersResponse;
 import com.bharath.ws.trainings.Order;
+import com.bharath.ws.trainings.Product;
 
 public class CustomerOrderWsClient {
 
-	public static void main(String[] args) throws MalformedURLException {
-		CustomerOrdersWsImplService service =  new CustomerOrdersWsImplService(new URL("http://localhost:8080/wsdlfirstws/customerordersservice?wsdl"));
-		CustomerOrdersPortType customerOrdersWsImplPort = service.getCustomerOrdersWsImplPort();
+	public static void main(String[] args) {
+		CustomerOrdersWsImplService service;
+		try {
+			service = new CustomerOrdersWsImplService(new URL("http://localhost:8080/wsdlfirstws/customerordersservice?wsdl"));
+			CustomerOrdersPortType customerOrdersWsImplPort = service.getCustomerOrdersWsImplPort();
+			
+			GetOrdersRequest request = new GetOrdersRequest();
+			request.setCustomerId(BigInteger.valueOf(1));
+			
+			GetOrdersResponse response = customerOrdersWsImplPort.getOrders(request);
+			List<Order> orders = response.getOrder();
+			for (Order order : orders) {
+				List<Product> products = order.getProduct();
+				for (Product product : products) {
+					System.out.println("Product Description " + product.getDescription());
+					System.out.println("Product Quantity " + product.getQuantity());
+				}
+				
+			}
 		
-		GetOrdersRequest request = new GetOrdersRequest();
-		request.setCustomerId(BigInteger.valueOf(1));
-		
-		GetOrdersResponse response = customerOrdersWsImplPort.getOrders(request);
-		List<Order> orders = response.getOrder();
-		System.out.println("Number of orders for the customers are" + orders.size());
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 
